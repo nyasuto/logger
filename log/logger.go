cf6b1365003340e35c4aefa067662afd3ac46f63
Yasuto Nishii <yast@me.com>
2023-04-21 21:46:23 +0900

    Create go.yml (#5)

--- log/logger.go
+++ log/logger.go
@@ -3,38 +3,37 @@ package log
 import "log"
 
 type CustomLogger interface {
-	Logger(format string, args ...interface{})
+	Logger(format string, args ...any)
 }
 
 type customLog struct {
-	output func(format string, args ...interface{})
+	output func(format string, args ...any)
 }
 
 func NewCustomLogger() *customLog {
 	return &customLog{output: log.Printf}
 }
 
-func Logger(format string, args ...interface{}) {
+func Logger(format string, args ...any) {
 	NewCustomLogger().Logger(format, args...)
 }
 
 // put prefix [ERROR] when the log argument contains go error
 // by default putting prefix [INFO]
-func (logger *customLog) Logger(format string, args ...interface{}) {
+func (logger *customLog) Logger(format string, args ...any) {
 	prefix := "[INFO] "
 
 	if args == nil {
 		format = prefix + format
-		log.Printf(format)
+		logger.output(format)
 	} else {
 		for _, arg := range args {
 			switch arg.(type) {
 			case error:
-				prefix = "[ERROR] "
-				break
+				prefix = "[ERROR] "				
 			}
 		}
 		format = prefix + format
-		log.Printf(format, args...)
+		logger.output(format, args...)
 	}
 }
